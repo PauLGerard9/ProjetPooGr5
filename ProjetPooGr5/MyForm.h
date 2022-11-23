4888a0341632cd4e424ccd03a6adaabeeed9fe7c
vico2903 <vicst29@gmail.com>
2022-11-23 11:48:34 +0100

    test12

--- ProjetPooGr5/MyForm.h
+++ ProjetPooGr5/MyForm.h
@@ -71,27 +71,31 @@ namespace ProjetPooGr5 {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(393, 155);
+			this->button1->Location = System::Drawing::Point(295, 126);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(56, 19);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Braut";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(659, 155);
+			this->button2->Location = System::Drawing::Point(494, 126);
+			this->button2->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->Size = System::Drawing::Size(56, 19);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Haaland";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(95, 155);
+			this->button3->Location = System::Drawing::Point(71, 126);
+			this->button3->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->Size = System::Drawing::Size(56, 19);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"Erling";
 			this->button3->UseVisualStyleBackColor = true;
@@ -99,27 +103,30 @@ namespace ProjetPooGr5 {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(273, 119);
+			this->label1->Location = System::Drawing::Point(205, 97);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(239, 17);
+			this->label1->Size = System::Drawing::Size(177, 13);
 			this->label1->TabIndex = 3;
 			this->label1->Text = L"joyeux anniversaire les petits indiens";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(393, 50);
+			this->label2->Location = System::Drawing::Point(295, 41);
+			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(163, 17);
+			this->label2->Size = System::Drawing::Size(123, 13);
 			this->label2->TabIndex = 4;
 			this->label2->Text = L"coucou les petits indiens";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(421, 246);
+			this->label3->Location = System::Drawing::Point(316, 200);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(136, 17);
+			this->label3->Size = System::Drawing::Size(102, 13);
 			this->label3->TabIndex = 5;
 			this->label3->Text = L"Victor est trop beau ";
 			// 
@@ -154,18 +161,16 @@ namespace ProjetPooGr5 {
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(808, 372);
-			this->Controls->Add(this->label6);
-			this->Controls->Add(this->label5);
-			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
@@ -173,5 +178,9 @@ namespace ProjetPooGr5 {
 
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
+
+// marche ta grosse darrone
