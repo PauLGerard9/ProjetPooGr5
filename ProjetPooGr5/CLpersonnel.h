58b2d1237f846726d52340697ccdeda5cfff8f80
vico2903 <vicst29@gmail.com>
2022-11-28 14:42:17 +0100

    test 15

--- ProjetPooGr5/CLpersonnel.h
+++ ProjetPooGr5/CLpersonnel.h
@@ -1,5 +1,19 @@
 #pragma once
+using namespace std;
 //un autre test de fou
-int prenom;
+class CLpersonnel {
+private:
+	int id_personnel;
+	int id_human;
+	string nom;
+	
+		
+
+
+public:
+
+};
+
+
 
 //march stp
