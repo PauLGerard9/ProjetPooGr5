6462cce94e92f5d79a2e50962de895e25d469662
VanitaKocher <116548317+VanitaKocher@users.noreply.github.com>
2022-11-29 13:53:09 +0100

    Update CLPaiements.h
    Signed-off-by: VanitaKocher <116548317+VanitaKocher@users.noreply.github.com>

--- ProjetPooGr5/CLPaiements.h
+++ ProjetPooGr5/CLPaiements.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "tools.h"
+
+class CLPaiements
+{
+private:
+	int id_paiement;
+    int montant_ht;
+    string mode_de_paiement;
+    string date_de_paiement;
+    string reference_commande;
+
+public:
+    set
+};
