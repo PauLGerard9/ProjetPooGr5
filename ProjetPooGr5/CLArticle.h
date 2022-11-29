5a835f46dee717402bb14cfb9b53fdc8235c0f67
VanitaKocher <116548317+VanitaKocher@users.noreply.github.com>
2022-11-29 09:37:46 +0100

    Update CLArticle.h
    Signed-off-by: VanitaKocher <116548317+VanitaKocher@users.noreply.github.com>

--- ProjetPooGr5/CLArticle.h
+++ ProjetPooGr5/CLArticle.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "tools.h"
+
+class CLArticle
+{
+private:
+	string reference_article;
+    string nom_article;
+    int quantite_en_stock;
+    int prix;
+    int seuil_reaprovisionnement;
+    double tva;
+public:
+};
