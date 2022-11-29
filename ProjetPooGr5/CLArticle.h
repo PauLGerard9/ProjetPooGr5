2fe00c88a29a546e38ed033d529079f57d87a4e1
VanitaKocher <116548317+VanitaKocher@users.noreply.github.com>
2022-11-29 10:12:23 +0100

    Update CLArticle.h
    Signed-off-by: VanitaKocher <116548317+VanitaKocher@users.noreply.github.com>

--- ProjetPooGr5/CLArticle.h
+++ ProjetPooGr5/CLArticle.h
@@ -12,4 +12,22 @@ private:
     	double tva;
 
 public:
+	string set_reference_article(string reference_article);
+	string set_nom_article(string nom_article);
+	int set_quantite_en_stock(int quantite_en_stock);
+	int set_prix_ht(int prix_ht);
+	int set_seuil_reaprovisionnement(int seuil_reaprovisionnement);
+	double set_tva(double tva);
+	
+	string get_reference_article();
+	string get_nom_article();
+	int get_quantite_en_stock();
+	int get_prix_ht();
+	int get_seuil_reaprovisionnement();
+	double get_tva();
+	
+	void creer_article();
+	void supprimer_article();
+	void modifier_article();
+	void afficher_article();
 };
