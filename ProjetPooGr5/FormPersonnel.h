9a402267c8446199110f7e49d121610f8ab180b7
Paul GERARD <paul.gerard@viacesi.fr>
2022-11-30 14:05:36 +0100

    commiutse

--- ProjetPooGr5/FormPersonnel.h
+++ ProjetPooGr5/FormPersonnel.h
@@ -35,6 +35,7 @@ namespace ProjetPooGr5 {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
 	protected:
 
 	protected:
@@ -53,6 +54,7 @@ namespace ProjetPooGr5 {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -64,11 +66,21 @@ namespace ProjetPooGr5 {
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(390, 55);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
 			// FormPersonnel
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(708, 339);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"FormPersonnel";
 			this->Text = L"FormPersonnel";
--- ProjetPooGr5/MyFormAccueil.h
+++ ProjetPooGr5/MyFormAccueil.h
@@ -243,11 +243,9 @@ private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) 
 }
 private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
 	this->Hide(); // sert a cacher la fenetre 
-	//ProjetPooGr5::MyForm MyForm23;
-	//MyForm23.ShowDialog(); // affiche 
-	this->Close();
-	FormPersonnel^ maForm1 = gcnew FormPersonnel();
+	FormPersonnel^ maForm1 = gcnew FormPersonnel(); 
 	maForm1->Show();
+	
 }
 };
 }
