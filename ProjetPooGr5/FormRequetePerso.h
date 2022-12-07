815dbd1de4750ee8bc946c8861b60993c478e798
Paul GERARD <paul.gerard@viacesi.fr>
2022-12-07 02:23:55 +0100

    j

--- ProjetPooGr5/FormRequetePerso.h
+++ ProjetPooGr5/FormRequetePerso.h
@@ -39,12 +39,13 @@ namespace ProjetPooGr5 {
 	private: NS_Comp_Svc::CLservice^ oSvc;
 	private: System::Data::DataSet^ oDs;
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::DataGridView^ dataGridView1;
+
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::TextBox^ txt_requete_perso;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::TextBox^ txt_action_perso;
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
 
 
 	protected:
@@ -64,14 +65,14 @@ namespace ProjetPooGr5 {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(FormRequetePerso::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->txt_requete_perso = (gcnew System::Windows::Forms::TextBox());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->txt_action_perso = (gcnew System::Windows::Forms::TextBox());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
@@ -85,16 +86,6 @@ namespace ProjetPooGr5 {
 			this->button1->Text = L"retour";
 			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &FormRequetePerso::button1_Click);
-			// 
-			// dataGridView1
-			// 
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(8, 15);
-			this->dataGridView1->Margin = System::Windows::Forms::Padding(4);
-			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->RowHeadersWidth = 51;
-			this->dataGridView1->Size = System::Drawing::Size(1259, 185);
-			this->dataGridView1->TabIndex = 1;
 			// 
 			// button2
 			// 
@@ -146,25 +137,35 @@ namespace ProjetPooGr5 {
 			this->pictureBox1->TabIndex = 29;
 			this->pictureBox1->TabStop = false;
 			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(12, 1);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(1263, 223);
+			this->dataGridView1->TabIndex = 30;
+			// 
 			// FormRequetePerso
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::LightBlue;
 			this->ClientSize = System::Drawing::Size(1283, 636);
+			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->txt_action_perso);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->txt_requete_perso);
 			this->Controls->Add(this->button2);
-			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->button1);
 			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->Name = L"FormRequetePerso";
 			this->Text = L"FormRequetePerso";
 			this->Load += gcnew System::EventHandler(this, &FormRequetePerso::FormRequetePerso_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
